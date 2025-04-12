0c6cc82e3cb90124769781f324c8bf7d2f4c9cc9
Habibur Rahman <softdev.habib@gmail.com>
2025-04-13 03:27:14 +0600

    first commit

---
+++ quick-cart-shopping.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* Plugin Name: Quick Cart Shopping
+* Plugin URI: https://giantwpsolutions.com/plugins/quick-cart-shopping
+* Description:  A modern WooCommerce UX booster — adds floating cart, variation popups, and drag & drop shopping experience. Convert more customers with smooth, app-like interactions.
+* Version: 1.0.0
+* Author: Giant WP Solutions
+* Author URI: https://giantwpsolutions.com
+* License: GPLv2 or later
+* Text Domain: quick-cart-shopping
+* WC requires at least: 3.0.0
+* WC tested up to: 9.5.1
+* Requires PHP: 7.4
+* WooCommerce HPOS support: yes
+* Domain Path: /languages
+* @package Quick Cart Shopping
+*/
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__  . '/app/functions.php';
+
+/**
+ * The main plugin class
+ */
+
+final class Quick_Cart_Shopping{
+
+      /**
+     * The plugin version
+     */
+    const version = '1.1.0';
+
+    
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'habib/quick-cart-shopping',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'habib/quick-cart-shopping' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
